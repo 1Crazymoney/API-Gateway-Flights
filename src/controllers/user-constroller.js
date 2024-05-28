fb56809a87de0ca2a8b3d9d5206266b7c1b61e2b
Ashutosh Shukla <ashutosh6471@gmail.com>
2024-05-28 22:09:12 +0530

    Added new user signup api

---
+++ src/controllers/user-constroller.js
@@ -0,0 +1,37 @@
+const {StatusCodes} = require('http-status-codes');
+const {UserService} = require('../services');
+const { response } = require('express');
+const { error } = require('winston');
+
+const {SuccessResponse,ErrorResponse} = require('../Utils/common');
+
+
+/**
+ * POST: /signup
+ * req-body {email:'sample-email@gmail.com',password:'Sample@2324'}
+ */
+
+async function signUp(req, res) {
+    try {
+        const user = await UserService.createUser({
+            email: req.body.email,
+            password: req.body.password
+        });
+        SuccessResponse.data = user;
+        return res
+            .status(StatusCodes.CREATED)
+            .json(SuccessResponse);
+    } catch (error) {
+        ErrorResponse.error = error;
+        return res
+            .status(error.statusCode)
+            .json(ErrorResponse);
+
+
+    }
+
+}
+module.exports = {
+    signUp
+
+}

bcd46bd69ddbbf83e94d1ebbe410ec3ff1be4e35
Ashutosh Shukla <ashutosh6471@gmail.com>
2024-05-29 22:52:49 +0530

    Added middlewares for auth

--- src/routes/V1/user-routes.js
+++ src/routes/V1/user-routes.js
@@ -2,14 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const {UserController} = require('../../controllers');
+const {AuthRequestMiddlewares} = require('../../middlewares');
 
 router
     .post('/signup',
+        AuthRequestMiddlewares.validateAuthRequest,
         UserController.signUp
 )
 
 router
     .post('/signin',
+        AuthRequestMiddlewares.validateAuthRequest,
         UserController.signIn
 )
 
